fce26f05c452d4457a92d9a54594aeb9c2778bee
Prasannata <aspire_pprasad@kpmgcomau.onmicrosoft.com>
2018-05-09 19:06:01 +0530

    Style added

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -14,15 +14,15 @@ public class EmployeeService {
 	public EmployeeService() {
 		Employee emp1 = new Employee();
 		emp1.setEmployeeId("1");
-		emp1.setPreTaxSalary(10000);
-		emp1.setPostTaxSalary(9000);
+		emp1.setPreTaxSalary(30000);
+		emp1.setPostTaxSalary(20000);
 		emp1.setTaxPercent(10);
 		employees.put("1", emp1);
 		
 		Employee emp2 = new Employee();
 		emp2.setEmployeeId("2");
-		emp2.setPreTaxSalary(200000);
-		emp2.setPostTaxSalary(80000);
+		emp2.setPreTaxSalary(50000);
+		emp2.setPostTaxSalary(40000);
 		emp2.setTaxPercent(20);
 		employees.put("2", emp2);
 		
